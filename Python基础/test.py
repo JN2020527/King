7812963ed267ef7468b2d342a121e54f887cc000
Kevien <18846416619@163.com>
2023-12-22 14:50:18 +0800

    新建了Python练习题

--- Python基础/test.py
+++ Python基础/test.py
@@ -1,1 +1,13 @@
-def http_errors
+my_list = [1, 2, 3, 4, 5]
+
+# 创建迭代器对象
+my_iterator = iter(my_list)
+
+# # 逐个访问元素
+# print(next(my_iterator))  # 输出: 1
+# print(next(my_iterator))  # 输出: 2
+# print(next(my_iterator))  # 输出: 3
+
+# 使用 for 循环遍历
+for item in my_iterator:
+    print(item)
